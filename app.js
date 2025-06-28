14ca4add0f8c30ab3c036ce2d2f7630872774906
redouane <redouanektm@hotmail.fr>
2025-06-28 18:50:41 +0200

    mise a jour express

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 const router = require('./routes/route');
 
